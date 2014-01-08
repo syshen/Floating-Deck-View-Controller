f4ee0eab2d817bd2cb03345795b592efe30dbf34
syshen <sysheen@gmail.com>
2014-01-08 09:07:31 +0800

    Initial Commit

---
+++ FloatUpDeck/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FloatUpDeck
+//
+//  Created by syshen on 1/8/14.
+//  Copyright (c) 2014 syshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FloatUpDeck/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FloatUpDeck
+//
+//  Created by syshen on 1/8/14.
+//  Copyright (c) 2014 syshen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
